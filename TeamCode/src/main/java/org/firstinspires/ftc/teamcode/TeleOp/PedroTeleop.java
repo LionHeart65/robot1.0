592e8173fa92ff81b57b0d41330b1487d6bb7d54
Ari Yellin-Levine <ariyellinlevine@gmail.com>
2025-04-02 14:57:26 -0400

    commenting stuff out

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/PedroTeleop.java
@@ -0,0 +1,226 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.localization.Pose;
+import com.pedropathing.pathgen.BezierCurve;
+import com.pedropathing.pathgen.BezierLine;
+import com.pedropathing.pathgen.Path;
+import com.pedropathing.pathgen.Point;
+import com.pedropathing.util.Constants;
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.teamcode.Utils.DriveBase;
+import org.firstinspires.ftc.teamcode.Utils.Intake;
+import org.firstinspires.ftc.teamcode.Utils.Robot;
+import org.firstinspires.ftc.teamcode.Utils.SparkFunOdo;
+import org.firstinspires.ftc.teamcode.pedroPathing.constants.FConstants;
+import org.firstinspires.ftc.teamcode.pedroPathing.constants.LConstants;
+
+@TeleOp(name = "Pedro Teleop")
+public class PedroTeleop extends LinearOpMode {
+
+    Robot robot;
+    DriveBase drive;
+    SparkFunOdo odo;
+    Intake intake;
+
+
+    private final Pose loadingZone = new Pose(26.919, 24.024, 5*Math.PI/4);
+    private final Pose destination = new Pose(118.963,119.686,Math.PI/4);
+    private final Pose destinationPass = new Pose(27.063,116.647,0);
+    private final Pose loadingZonePass = new Pose(128.804, 30.971, 0);
+
+    private Follower follower;
+
+    private Gamepad.RumbleEffect sos;
+    private String actionState = "Outtake";
+
+    boolean reverseSwitch, sensSwitch, reset, autoMove, stateSwitch;
+    int pathState;
+
+
+    @Override
+    public void runOpMode() {
+        initialize();
+        waitForStart();
+
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+                update();
+
+                if (autoMove && (Math.abs(gamepad1.left_stick_x) > 0 || Math.abs(gamepad1.right_stick_x) > 0 || Math.abs(gamepad1.left_stick_y) > 0)) {
+                    autoMove = false;
+                }
+
+                if (!autoMove) {
+                    drive.moveAbs(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
+                }
+
+
+                //GPad 2 A: Reverses spin
+                if (gamepad2.a) {
+                    if (!reverseSwitch) {
+                        intake.reverse();
+                        reverseSwitch = true;
+                    }
+
+                } else {
+                    reverseSwitch = false;
+                }
+
+                //GPad 2 Back: reset field centric
+                if (gamepad2.back) {
+                    if (!reset) {
+                        reset = true;
+                        odo.reset();
+                    }
+                } else {
+                    reset = false;
+                }
+
+                //GPad 2 X: switches action state
+                if (gamepad2.x) {
+                    if (!stateSwitch) {
+                        stateSwitch = true;
+                        switchActionState();
+                    }
+                } else {
+                    stateSwitch = false;
+                }
+
+
+                //Gpad1 Left Bumper: toggles sensitivity
+                if (gamepad1.left_bumper) {
+                    if (!sensSwitch) {
+                        sensSwitch = true;
+                        if (drive.sensitivity == 1) {
+                            drive.setSensitivity(0.5);
+                        } else {
+                            drive.setSensitivity(1);
+                        }
+                    }
+                } else {
+                    sensSwitch = false;
+                }
+
+                //Gpad 1 X: destination move
+                if (gamepad1.x) {
+                    if (actionState.equals("Outtake")) {
+                        setAutoMove(destination, destinationPass);
+                    } else if (actionState.equals("Intake")) {
+                        setAutoMove(loadingZone, destinationPass);
+                    } else {
+                        gamepad1.runRumbleEffect(sos);
+                    }
+                }
+
+                //Gpad 1 Y: Loading move
+                if (gamepad1.y) {
+                    if (actionState.equals("Outtake")) {
+                        setAutoMove(destination, loadingZonePass);
+                    } else if (actionState.equals("Intake")) {
+                        setAutoMove(loadingZone, loadingZonePass);
+                    } else {
+                        gamepad1.runRumbleEffect(sos);
+                    }
+                }
+
+
+
+                //Gpad 1 B: destination move
+                if (gamepad1.b) {
+                    if (actionState.equals("Outtake")) {
+                        setAutoMove(destination);
+                    } else if (actionState.equals("Intake")) {
+                        setAutoMove(loadingZone);
+                    } else {
+                        gamepad1.runRumbleEffect(sos);
+                    }
+                }
+
+                if (autoMove) {
+                    if (!follower.isBusy()) {
+                        autoMove = false;
+                        switchActionState();
+                    }
+                    telemetry.addLine("AutoMoving");
+                    follower.update();
+                }
+
+                telemetry.update();
+            }
+        }
+    }
+
+    private void switchActionState() {
+        if (actionState.equals("Outtake")) {
+            actionState = "Intake";
+        } else if (actionState.equals("Intake")) {
+            actionState = "Outtake";
+        }
+    }
+
+
+    private void setAutoMove(Pose endPos) {
+        Path path;
+        SparkFunOTOS.Pose2D robotPos = odo.getPos();
+
+        path = new Path(new BezierLine(new Point(new Pose(robotPos.x, robotPos.y, robotPos.h)), new Point(endPos)));
+        path.setLinearHeadingInterpolation(odo.getPos().h, endPos.getHeading());
+
+
+
+        follower.followPath(path);
+        telemetry.addLine("New Path");
+
+        autoMove = true;
+    }
+
+    private void setAutoMove(Pose endPos, Pose passPoint) {
+        Path path;
+        SparkFunOTOS.Pose2D robotPos = odo.getPos();
+
+        path = new Path(new BezierCurve(new Point(new Pose(robotPos.x, robotPos.y, robotPos.h)), new Point(passPoint), new Point(endPos)));
+        path.setLinearHeadingInterpolation(odo.getPos().h, endPos.getHeading());
+
+        follower.followPath(path);
+
+        autoMove = true;
+
+    }
+    public void initialize() {
+        robot = new Robot(hardwareMap, new Pose(9.2,33.2,-Math.PI/2));
+        drive = new DriveBase(robot, gamepad1);
+        odo = robot.odo;
+        intake = new Intake(robot);
+
+        Constants.setConstants(FConstants.class, LConstants.class);
+        follower = new Follower(hardwareMap);
+        follower.setStartingPose(robot.startPos);
+
+        sos = new Gamepad.RumbleEffect.Builder()
+                .addStep(1, 1, 100).addStep(1, 1, 100).addStep(1, 1, 100)
+                .addStep(1,1,300).addStep(1,1,300).addStep(1,1,300)
+                .addStep(1, 1, 100).addStep(1, 1, 100).addStep(1, 1, 100).build();
+    }
+
+    public void update() {
+        SparkFunOTOS.Pose2D pos = odo.getPos();
+        telemetry.addLine("Gamepad 2: A: reverse intake, Back: Resets fieldcentric, X: Switches actionState");
+
+        telemetry.addLine("Gamepad 1: Left Bumper: sensitivity toggle, X: Destination Move, Y: Loading Move, B: Move without control point");
+
+        telemetry.addData("X: ", pos.x);
+        telemetry.addData("Y: ", pos.y);
+        telemetry.addData("Heading: ", pos.h);
+        telemetry.addData("Sensitivity: ", drive.sensitivity);
+        telemetry.addData("Direction: ", intake.getDirection());
+        telemetry.addData("Action State: ", actionState);
+        telemetry.addData("Current Path", follower.getCurrentPath());
+        intake.update();
+    }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/RedTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/RedTeleop.java
@@ -1,23 +1,14 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
-import com.pedropathing.follower.Follower;
 import com.pedropathing.localization.Pose;
-import com.pedropathing.pathgen.BezierCurve;
-import com.pedropathing.pathgen.BezierLine;
-import com.pedropathing.pathgen.Path;
-import com.pedropathing.pathgen.Point;
-import com.pedropathing.util.Constants;
 import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.teamcode.Utils.DriveBase;
 import org.firstinspires.ftc.teamcode.Utils.Intake;
 import org.firstinspires.ftc.teamcode.Utils.Robot;
 import org.firstinspires.ftc.teamcode.Utils.SparkFunOdo;
-import org.firstinspires.ftc.teamcode.pedroPathing.constants.FConstants;
-import org.firstinspires.ftc.teamcode.pedroPathing.constants.LConstants;
 
 @TeleOp(name = "Red Teleop")
 public class RedTeleop extends LinearOpMode {
@@ -27,20 +18,12 @@ public class RedTeleop extends LinearOpMode {
     SparkFunOdo odo;
     Intake intake;
 
+    private final Pose loadingZone = new Pose();
+    private final Pose destination = new Pose();
+    private final Pose circle = new Pose();
 
-    private final Pose loadingZone = new Pose(26.919, 24.024, 5*Math.PI/4);
-    private final Pose destination = new Pose(118.963,119.686,Math.PI/4);
-    private final Pose destinationPass = new Pose(27.063,116.647,0);
-    private final Pose loadingZonePass = new Pose(128.804, 30.971, 0);
 
-    private Follower follower;
-
-    private Gamepad.RumbleEffect sos;
-    private String actionState = "Outtake";
-
-    boolean reverseSwitch, sensSwitch, reset, autoMove, stateSwitch;
-    int pathState;
-
+    boolean reverseSwitch, sensSwitch, reset;
 
     @Override
     public void runOpMode() {
@@ -51,15 +34,8 @@ public class RedTeleop extends LinearOpMode {
             while (opModeIsActive()) {
                 update();
 
-                if (autoMove && (Math.abs(gamepad1.left_stick_x) > 0 || Math.abs(gamepad1.right_stick_x) > 0 || Math.abs(gamepad1.left_stick_y) > 0)) {
-                    autoMove = false;
-                }
-
-                drive.moveAbs(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
-
-
-                //GPad 2 A: Reverses spin
-                if (gamepad2.a) {
+                //A: Reverses spin
+                if (gamepad1.a) {
                     if (!reverseSwitch) {
                         intake.reverse();
                         reverseSwitch = true;
@@ -68,29 +44,7 @@ public class RedTeleop extends LinearOpMode {
                 } else {
                     reverseSwitch = false;
                 }
-
-                //GPad 2 Back: reset field centric
-                if (gamepad2.back) {
-                    if (!reset) {
-                        reset = true;
-                        odo.reset();
-                    }
-                } else {
-                    reset = false;
-                }
-
-                //GPad 2 X: switches action state
-                if (gamepad2.x) {
-                    if (!stateSwitch) {
-                        stateSwitch = true;
-                        switchActionState();
-                    }
-                } else {
-                    stateSwitch = false;
-                }
-
-
-                //Gpad1 Left Bumper: toggles sensitivity
+                //Left Bumper: toggles sensitivity
                 if (gamepad1.left_bumper) {
                     if (!sensSwitch) {
                         sensSwitch = true;
@@ -103,126 +57,37 @@ public class RedTeleop extends LinearOpMode {
                 } else {
                     sensSwitch = false;
                 }
-
-                //Gpad 1 X: destination move
-                if (gamepad1.x) {
-                    if (actionState.equals("Outtake")) {
-                        setAutoMove(destination, destinationPass);
-                    } else if (actionState.equals("Intake")) {
-                        setAutoMove(loadingZone, destinationPass);
-                    } else {
-                        gamepad1.runRumbleEffect(sos);
+                //Back: reset field centric
+                if (gamepad1.back) {
+                    if (!reset) {
+                        reset = true;
+                        odo.reset();
                     }
+                } else {
+                    reset = false;
                 }
-
-                //Gpad 1 Y: Loading move
-
-                if (gamepad1.y) {
-                    if (actionState.equals("Outtake")) {
-                        setAutoMove(destination, loadingZonePass);
-                    } else if (actionState.equals("Intake")) {
-                        setAutoMove(loadingZone, loadingZonePass);
-                    } else {
-                        gamepad1.runRumbleEffect(sos);
-                    }
-                }
-
-
-
-                //Gpad 1 B: destination move
-                if (gamepad1.b) {
-                    if (actionState.equals("Outtake")) {
-                        setAutoMove(destination);
-                    } else if (actionState.equals("Intake")) {
-                        setAutoMove(loadingZone);
-                    } else {
-                        gamepad1.runRumbleEffect(sos);
-                    }
-                }
-
-                if (autoMove) {
-                    if (!follower.isBusy()) {
-                        autoMove = false;
-                        switchActionState();
-                    }
-                    telemetry.addLine("AutoMoving");
-                    follower.update();
-                }
-
                 telemetry.update();
             }
         }
     }
 
-    private void switchActionState() {
-        if (actionState.equals("Outtake")) {
-            actionState = "Intake";
-        } else if (actionState.equals("Intake")) {
-            actionState = "Outtake";
-        }
-    }
-
-
-    private void setAutoMove(Pose endPos) {
-        Path path;
-        SparkFunOTOS.Pose2D robotPos = odo.getPos();
-
-        path = new Path(new BezierLine(new Point(new Pose(robotPos.x, robotPos.y, robotPos.h)), new Point(endPos)));
-        path.setLinearHeadingInterpolation(odo.getPos().h, endPos.getHeading());
-
-
-
-        follower.followPath(path);
-        telemetry.addLine("New Path");
-
-        autoMove = true;
-    }
-
-    private void setAutoMove(Pose endPos, Pose passPoint) {
-        Path path;
-        SparkFunOTOS.Pose2D robotPos = odo.getPos();
-
-        path = new Path(new BezierCurve(new Point(new Pose(robotPos.x, robotPos.y, robotPos.h)), new Point(passPoint), new Point(endPos)));
-        path.setLinearHeadingInterpolation(odo.getPos().h, endPos.getHeading());
-
-        follower.followPath(path);
-
-//        autoMove = true;
-
-    }
     public void initialize() {
-        robot = new Robot(hardwareMap, new Pose(9.2,33.2,-Math.PI/2));
+        robot = new Robot(hardwareMap, new Pose(0,0, -Math.PI/2));
         drive = new DriveBase(robot, gamepad1);
         odo = robot.odo;
         intake = new Intake(robot);
-
-        Constants.setConstants(FConstants.class, LConstants.class);
-        follower = new Follower(hardwareMap);
-        follower.setStartingPose(robot.startPos);
-
-        sos = new Gamepad.RumbleEffect.Builder()
-                .addStep(1, 1, 100).addStep(1, 1, 100).addStep(1, 1, 100)
-                .addStep(1,1,300).addStep(1,1,300).addStep(1,1,300)
-                .addStep(1, 1, 100).addStep(1, 1, 100).addStep(1, 1, 100).build();
     }
 
-
-
     public void update() {
+        drive.moveAbs(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
         SparkFunOTOS.Pose2D pos = odo.getPos();
-        telemetry.addLine("Gamepad 2: A: reverse intake, Back: Resets fieldcentric, X: Switches actionState");
-
-        telemetry.addLine("Gamepad 1: Left Bumper: sensitivity toggle, X: Destination Move, Y: Loading Move, B: Move without control point");
+        telemetry.addData("A: reverse intake", "left bumper: sensitivity toggle, Back: Resets fieldcentric");
 
         telemetry.addData("X: ", pos.x);
         telemetry.addData("Y: ", pos.y);
         telemetry.addData("Heading: ", pos.h);
         telemetry.addData("Sensitivity: ", drive.sensitivity);
         telemetry.addData("Direction: ", intake.getDirection());
-        telemetry.addData("Action State: ", actionState);
-        telemetry.addData("Current Path", follower.getCurrentPath());
         intake.update();
     }
-
-
 }
