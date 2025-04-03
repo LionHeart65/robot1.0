f1ac8de61ca76caa38acb7f63b46c1e68ef5e293
YertleTheTurtle <60442578+yertlethetertle66@users.noreply.github.com>
2025-04-03 15:06:42 -0400

    LED implementation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Robot.java
@@ -7,10 +7,12 @@ import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 
 public class Robot {
     public final DcMotor frontLeft, backLeft, frontRight, backRight;
     public DcMotor intakeMotor;
+    public DigitalChannel LED;
 
     public Pose startPos;
     public final SparkFunOdo odo;
@@ -26,6 +28,9 @@ public class Robot {
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
         intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
+
+        LED = hardwareMap.get(DigitalChannel.class, "LED");
+        LED.setMode(DigitalChannel.Mode.OUTPUT);
 
         odo = new SparkFunOdo(hardwareMap, new SparkFunOTOS.Pose2D(-6.49606,0,0), new SparkFunOTOS.Pose2D(startPos.getX(), startPos.getY(), startPos.getHeading()));
 
